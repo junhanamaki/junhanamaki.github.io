57b885088fae088bb384a5a62d8ca9fdc333399b
junhanamaki <jun.hanamaki@gmail.com>
2016-02-07 10:59:44 +0000

    animations

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,36 @@
-function onNavigatorClick() {
-  $('.container').removeClass('centered-origin');
-  $('.wrapper').removeClass('centered-content');
+(function() {
+  let state = 0;
 
-  $('#my-name').removeClass('positioned-my-name');
-  $('#my-title').removeClass('positioned-my-title');
-  $('#my-links').removeClass('positioned-my-links');
-  $('#my-avatar').removeClass('positioned-my-avatar');
-}
+  window.onNavigatorClick = function() {
+    if (state == 0) {
+      navigateToCV();
+
+      state = 1;
+    } else {
+      navigateToIntro()
+
+      state = 0;
+    }
+  };
+
+  function navigateToCV() {
+    $('.container').removeClass('centered-origin');
+    $('.wrapper').removeClass('centered-content');
+
+    $('#my-name').removeClass('positioned-my-name');
+    $('#my-title').removeClass('positioned-my-title');
+    $('#my-links').removeClass('positioned-my-links');
+    $('#my-avatar').removeClass('positioned-my-avatar');
+  }
+
+  function navigateToIntro() {
+    $('#my-name').addClass('positioned-my-name');
+    $('#my-title').addClass('positioned-my-title');
+    $('#my-links').addClass('positioned-my-links');
+    $('#my-avatar').addClass('positioned-my-avatar');
+
+
+    $('.wrapper').addClass('centered-content');
+    $('.container').addClass('centered-origin');
+  }
+})();
